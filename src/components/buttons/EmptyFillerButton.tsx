9c5ea1eba51d50d1fcc0a9c194c821b417c4aceb
Adam <62818816+Beschuetzer@users.noreply.github.com>
2023-11-09 09:58:26 -0600

    Fix issue with EmptyFillerButton not restting cursor style

--- src/components/buttons/EmptyFillerButton.tsx
+++ src/components/buttons/EmptyFillerButton.tsx
@@ -2,5 +2,9 @@ import { CLASSNAME__BUTTON } from "../../constants";
 import React from "react";
 
 export const EmptyFillerButton = () => {
-  return <div className={CLASSNAME__BUTTON} />;
-};
+  return <div className={CLASSNAME__BUTTON} ref={(node) => {
+     if (node) {
+       node.style.setProperty("cursor", "auto", "important");
+     }
+  }} />
+}
