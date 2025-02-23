57c7dd38b5c5d94212f84fa6500f50b547c43882
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-02-23 11:39:24 -0600

    chore: fix new story

--- src/stories/PaddingAndMargin.stories.tsx
+++ src/stories/PaddingAndMargin.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Carousel } from "../components/Carousel";
 import { items } from "./assets/items";
+import { container } from "./decorators";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -11,6 +12,7 @@ const meta = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
   },
+  decorators: container,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
@@ -28,12 +30,58 @@ export const Story1: Story = {
       layout: {
         itemDisplayLocation: "above",
       },
-      container: {
-        style: {
-            margin: 14
+      styling: {
+        container: {
+          margin: {
+            top: 14,
+            right: 14,
+            bottom: 14,
+            left: 14,
+          }
         }
       }
     },
   },
-  name: "containter.style.margin 14",
+  name: "styling.container.margin 14",
+};
+
+export const Story2: Story = {
+  args: {
+    items: items.slice(0, items.length - 1),
+    options: {
+      layout: {
+        itemDisplayLocation: "above",
+      },
+      styling: {
+        container: {
+          padding: {
+            top: 14,
+            right: 14,
+            left: 14,
+          }
+        }
+      }
+    },
+  },
+  name: "styling.container.padding 14",
+};
+
+export const Story3: Story = {
+  args: {
+    items: items.slice(0, items.length - 1),
+    options: {
+      layout: {
+        itemDisplayLocation: "above",
+      },
+      styling: {
+        itemViewer: {
+          padding: {
+            right: 14,
+            left: 14,
+          }
+        }
+      }
+    },
+  },
+  name: "styling.itemViewer.padding 14",
 };
